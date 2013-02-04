ff1b11988079e0342fe080e6bc7c0956af0672d2
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-04 11:03:27 +0100

    Add bitstr_to_list, enhance hello world.

--- main.erl
+++ main.erl
@@ -2,4 +2,18 @@
 -export([start/1]).
 
 start([Name]) ->
-    io:format("Hello ~s!~n", [Name]).
+    io:format("~w~nHello ~s!~n",
+              [bitstr_to_list(atom_to_binary(Name, latin1)), Name]).
+
+bitstr_to_list(<<N:1,Rest/bitstring>>) ->
+    [N | bitstr_to_list(Rest)];
+bitstr_to_list(<<>>) ->
+    [].
+
+%% (1) choose a genetic representation
+%% (2) build a population
+%% (3) design a fitness function
+%% (4) choose a selection operator
+%% (5) choose a recombination operator
+%% (6) choose a mutation operator
+%% (7) devise a data analysis procedure.
