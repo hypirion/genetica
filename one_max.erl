81ea9de10ec9622a80a5ac8648eaa245ff4999e1
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-09 16:42:25 +0100

    Refactor one-max, move random_bit to utils.

--- one_max.erl
+++ one_max.erl
@@ -1,25 +1,22 @@
 -module(one_max).
 -compile(export_all). %% TODO: Remove when finished.
 
-random_genotype_fn([N]) ->
+random_genotype_fn([N | _]) ->
     fun () ->
-            utils:repeatedly(N, fun random_bit/0)
+            conversions:list_to_bitstr(
+              utils:repeatedly(N, fun utils:random_bit/0))
     end.
 
-%% Utility fn
-random_bit() ->
-    random:uniform(2) - 1.
-
-genotype_to_phenotype_fn([_]) ->
+phenotype_to_genotype_fn(_) ->
     fun conversions:list_to_bitstr/1.
 
-phenotype_to_genotype_fn([_]) ->
+genotype_to_phenotype_fn(_) ->
     fun conversions:bitstr_to_list/1.
 
-evaluate_fitness_fn({phen_to_gen, F, init_vals, [N]}) ->
+fitness_fn({phen_to_gen, F, init_vals, [N]}) ->
     fun (Pheno) ->
             Geno = F(Pheno),
-            lists:sum(Geno) - abs(N - bit_size(Pheno))
+            lists:sum(Geno) - 2*abs(N - bit_size(Pheno))
     end.
     
 recombination_fn() ->
--- utils.erl
+++ utils.erl
@@ -1,7 +1,10 @@
 -module(utils).
--export([repeatedly/2]).
+-export([repeatedly/2, random_bit/0]).
 
 repeatedly(0, _) ->
     [];
 repeatedly(N, Fun) ->
     [Fun() | repeatedly(N - 1, Fun)].
+
+random_bit() ->
+    random:uniform(2) - 1.
