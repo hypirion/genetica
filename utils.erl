54d633c8c816ef2cf3eab77302aba9693b62de75
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-09 17:42:31 +0100

    Implement average and standard deviation.

--- utils.erl
+++ utils.erl
@@ -1,5 +1,5 @@
 -module(utils).
--export([repeatedly/2, random_bit/0]).
+-export([repeatedly/2, random_bit/0, avg/1, std_dev/1, std_dev/2]).
 
 repeatedly(0, _) ->
     [];
@@ -8,3 +8,20 @@ repeatedly(N, Fun) ->
 
 random_bit() ->
     random:uniform(2) - 1.
+
+avg(Xs) ->
+    avg(Xs, 0, 0).
+
+avg([H | T], Len, Sum) ->
+    avg(T, Len + 1, Sum + H);
+avg([], Len, Sum) ->
+    Sum / Len.
+
+std_dev(Xs) ->
+    std_dev(Xs, avg(Xs)).
+
+std_dev(Xs, Avg) ->
+    Sums = lists:foldl(
+             fun(V, Acc) -> D = V - Avg, Acc + (D * D) end,
+             0, Xs),
+    math:sqrt(Sums / (length(Xs) - 1)).
