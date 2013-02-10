30bd189e6fad0c735bf96225797921ab55c72ac2
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-10 20:29:05 +0100

    Add function which fetches module fns.

--- main.erl
+++ main.erl
@@ -7,6 +7,16 @@ start([Name]) ->
     Geno = (one_max:random_genotype_fn([20]))(),
     io:format("~w~nHello ~s!~n", [Geno, Name]).
 
+fetch_fns(Module, Opts) ->
+    Rand_gtype = Module:random_genotype_fn(Opts),
+    PtoG = Module:phenotype_to_genotype_fn(Opts),
+    GtoP = Module:genotype_to_phenotype_fn(Opts),
+    Fitness = Module:fitness_fn(Opts),
+    Crossfn = Module:crossover_fn(Opts),
+    Mutfn = Module:mutation_fn(Opts),
+    {rand, Rand_gtype, p_to_g, PtoG, g_to_p, GtoP,
+     fitness, Fitness, cross, Crossfn, mut, Mutfn}.
+
 
 %% (1) choose a genetic representation
 %% (2) build a population
