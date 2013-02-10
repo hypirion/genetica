8190a0153520c460904084d2c56c8e90e9e20f48
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-10 01:47:52 +0100

    Implement mutation function.

--- one_max.erl
+++ one_max.erl
@@ -14,13 +14,26 @@ genotype_to_phenotype_fn(_) ->
     fun conversions:bitstr_to_list/1.
 
 fitness_fn({phen_to_gen, F, init_vals, [N]}) ->
-    fun (Pheno) ->
+    fun ({ptype, Pheno, others, _}) ->
             Geno = F(Pheno),
             lists:sum(Geno) - 2*abs(N - bit_size(Pheno))
     end.
     
-recombination_fn() ->
-    todo.
+crossover_fn(_) ->
+    fun (_G1, _G2) ->
+            1
+    end.
 
-mutation_fn() ->
-    todo.
+mutation_fn([P]) ->
+    Rec = fun (F, <<N:1,Rest/bitstring>>) ->
+                  Rmut = F(F, Rest/bitstring),
+                  case random:uniform() =< P of
+                      true -> Mutation = utils:random_bit(),
+                              <<Mutation:1, Rmut/bitstring>>;
+                      false -> <<N:1, Rmut/bitstring>>
+                  end;
+              (_, <<>>) -> <<>>
+          end,
+    fun (Geno) ->
+            Rec(Rec, Geno)
+    end.
