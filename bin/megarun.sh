81675992df60df2667bd322342f76c391f9d74c2
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-13 22:37:34 +0100

    Do megarun over other vals.

--- bin/megarun.sh
+++ bin/megarun.sh
@@ -1,14 +1,22 @@
 #! /bin/sh
 
+ITERS=100
+
 mkdir -p plots
-for mp in $(seq 0 0.1 1.0); do
-    for mbit in 0.01 0.05 0.1 0.25 0.5; do
-        for xo in $(seq 0.5 0.05 1.0); do
-            bin/multirun.sh 100 plots/m$mp-mb$mbit-cr$xo $mp $mbit $xo &
-            echo -n .
-        done
-        wait $!
+for S in rank boltzmann sigma; do
+    for C in full_replacement over_production generational_mixing; do
+        if [ "$C" != "full_replacement" ]; then
+            for I in $(seq 10 10 110); do
+                echo bin/multirun.sh $ITERS plots/r-$S-$C-$I- $S $C $I
+                bin/multirun.sh $ITERS plots/r-$S-$C-$I- $S $C $I &
+            done
+            wait $!
+        else
+            echo bin/multirun.sh $ITERS plots/r-$S-$C- $S $C 10
+            bin/multirun.sh $ITERS plots/r-$S-$C- $S $C 10 &
+        fi
     done
+    wait $!
 done
 echo
 echo done!
