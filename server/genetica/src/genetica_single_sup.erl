390c1606a20390d54cb40d900496ed415a1e834f
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-18 19:18:54 +0100

    Generate genetica app structure.

---
+++ server/genetica/src/genetica_single_sup.erl
@@ -0,0 +1,75 @@
+%%%-------------------------------------------------------------------
+%%% File    : genetica_single_sup.erl
+%%% Author  : Jean Niklas L'orange <jeannikl@hypirion.com>
+%%% Description : 
+%%%
+%%% Created : 18 Mar 2013 by Jean Niklas L'orange <jeannikl@hypirion.com>
+%%%-------------------------------------------------------------------
+-module(genetica_single_sup).
+
+-behaviour(supervisor).
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% External exports
+%%--------------------------------------------------------------------
+-export([
+         start_link/0
+        ]).
+
+%%--------------------------------------------------------------------
+%% Internal exports
+%%--------------------------------------------------------------------
+-export([
+         init/1
+        ]).
+
+%%--------------------------------------------------------------------
+%% Macros
+%%--------------------------------------------------------------------
+-define(SERVER, ?MODULE).
+
+%%--------------------------------------------------------------------
+%% Records
+%%--------------------------------------------------------------------
+
+%%====================================================================
+%% External functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link/0
+%% Description: Starts the supervisor
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Server functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok,  {SupFlags,  [ChildSpec]}} |
+%%          ignore                          |
+%%          {error, Reason}   
+%%--------------------------------------------------------------------
+init([]) ->
+    %% TODO: How to terminate supervisor when we're done? The transport worker
+    %% has to figure out when we're done, and terminate its supervisor! (Must be
+    %% a cast, as we'll have to respond to the shutdown call the supervisor
+    %% sends us)
+    TransportWorker = {transport,
+                       {genetica_transport_worker, start_link, []},
+                       %% Hurr, need to pass the communication layer.
+                       temporary, 2000, worker, [genetica_transport_worker]},
+    ComputeWorker = {compute,
+                     {genetica_compute_worker, start_link, []},
+                     %% Need to connect this to the transport worker...
+                     temporary, 2000, worker, [genetica_compute_worker]},
+    {ok,{{one_for_all, 0, 1}, %% Terminate everything if something fails
+         [TransportWorker, ComputeWorker]}}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
---
+++ server/genetica/src/genetica_sup.erl
@@ -0,0 +1,35 @@
+-module(genetica_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    RequestHandler = {request_handler,
+                      {genetica_request_handler, start_link, []},
+                      permanent, 5000, worker, [genetica_request_handler]},
+    ClientSupervisor = {client_supervisor,
+                        {genetica_client_sup, start_link, []}
+                        permanent, 5000, supervisor, [genetica_client_sup]},
+    RestartStrategy = {one_for_rest, 0, 1}, %% set to 4, 3600 later on.
+    {ok, {RestartStrategy, [ClientSupervisor, RequestHandler]}}.
